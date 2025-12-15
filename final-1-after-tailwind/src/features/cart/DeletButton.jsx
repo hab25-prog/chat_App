2ff6f991f255747bcb136bdc3c3de280bf45b1bb
hab25-prog <habtaassefa0@gmail.com>
2025-12-16 02:55:14 +0300

    Delete final-1-after-tailwind directory

--- final-1-after-tailwind/src/features/cart/DeletButton.jsx
+++
@@ -1,9 +0,0 @@
-import { useDispatch } from "react-redux";
-import Button from "../../ui/Button";
-import { deleteItem } from "./cartSlice";
-
-function DeletButton({pizzaId}){
-    const dispatch=useDispatch()
-    return <Button onClick={()=>dispatch(deleteItem(pizzaId))}   type="small">Delete</Button>
-}
-export default DeletButton
