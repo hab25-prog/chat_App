3499cbbc77e5f4c1ba5cee16c80fd7eb3660f623
Habtamu Assefa <habtaassefa0@gmail.com>
2025-12-16 03:35:33 +0300

    fix: replace production catch-all with middleware fallback to fix path-to-regexp error

--- chat-App/backend/src/index.js
+++ chat-App/backend/src/index.js
@@ -1,19 +1,27 @@
 // const express = require("express");
 import express from "express";
 // const dotenv = require("dotenv");
+import Path from "path";
 import dotenv from "dotenv";
-// const authRoutes = require("./routes/auth.route");
+
 import authRoutes from "./routes/auth.route.js";
 import messageRouter from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
+const _dirname = Path.resolve();
 const port = process.env.PORT;
 
 console.log(port);
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRouter);
-
+// make ready for deploymment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(Path.join(_dirname, "../frontend/dist")));
+  app.use((req, res) => {
+    res.sendFile(Path.join(_dirname, "../frontend", "dist", "index.html"));
+  });
+}
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
